f31944c60959fb0546fb5bfa7c56896c8e5f615c
Ben Wetherfield <bwetherfield@gmail.com>
2019-07-01 01:22:50 -0700

    Add Box cases to EnumBox

--- Sources/XMLCoder/Auxiliaries/Box/EnumBox.swift
+++ Sources/XMLCoder/Auxiliaries/Box/EnumBox.swift
@@ -7,18 +7,42 @@
 
 protocol EnumBox: Box {
     
-    associatedtype Name: CodingKey
-    var name: Name { get }
+    var name: CodingKey { get }
     
-    static func unboxed (as name: Name) -> Box
+    static func unboxed (as name: CodingKey) -> KeyedBox?
+    static func unboxed (as name: CodingKey) -> UnkeyedBox?
+    static func unboxed (as name: CodingKey) -> SharedBox<KeyedBox>?
+    static func unboxed (as name: CodingKey) -> SharedBox<UnkeyedBox>?
+    static func unboxed (as name: CodingKey) -> SimpleBox?
+
 }
 
 extension EnumBox {
     var isNull: Bool {
-        return false
+        if let box = Self.unboxed(as: name) as KeyedBox? {
+            return box.isNull
+        } else if let box = Self.unboxed(as: name) as UnkeyedBox? {
+            return box.isNull
+        } else if let box = Self.unboxed(as: name) as SharedBox<KeyedBox>? {
+            return box.isNull
+        } else if let box = Self.unboxed(as: name) as SharedBox<UnkeyedBox>? {
+            return box.isNull
+        } else if let box = Self.unboxed(as: name) as SimpleBox? {
+            return box.isNull
+        } else { return true }
     }
     
     func xmlString() -> String? {
-        return Self.unboxed(as: name).xmlString()
+        if let box = Self.unboxed(as: name) as KeyedBox? {
+            return box.xmlString()
+        } else if let box = Self.unboxed(as: name) as UnkeyedBox? {
+            return box.xmlString()
+        } else if let box = Self.unboxed(as: name) as SharedBox<KeyedBox>? {
+            return box.xmlString()
+        } else if let box = Self.unboxed(as: name) as SharedBox<UnkeyedBox>? {
+            return box.xmlString()
+        } else if let box = Self.unboxed(as: name) as SimpleBox? {
+            return box.xmlString()
+        } else { return nil }
     }
 }
